4049d21ac7868d7ad1c743e0740fd858f91fd9f2
Jonas Rafael <jonasrafael@hotmail.com.br>
2021-01-20 16:23:05 -0300

    Add others actions in Controller

--- Controllers/PedidoController.cs
+++ Controllers/PedidoController.cs
@@ -8,5 +8,20 @@ namespace ProjectMVC.Controllers
         {
             return View();
         }
+
+        public IActionResult Carrinho()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Resumo()
+        {
+            return View();
+        }
     }
 }
